b63ef18146d1a821ff0c29e494043340fcb4972f
horatiu.dan <horatiu.dan@tangoe.com>
2026-02-06 13:14:49 +0200

    Adds a second mcp server, enhances the MCP client configuration.

--- telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
+++ telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ai.mcp.client.common.autoconfigure.properties.McpStreamableHttpClientProperties;
 import org.springframework.ai.mcp.customizer.McpSyncClientCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
