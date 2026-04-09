e722a1b90559dec4b942f43731e26025306692e4
horatiu.dan <horatiu.dan@tangoe.com>
2026-04-09 12:33:43 +0300

    State: AI Assistant, 2 MCP servers, connected, not secured To Do: Secure the servers and the communication with API Keys.

--- telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
+++ telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
@@ -5,11 +5,9 @@ import com.hcd.telecomassistant.config.resolver.InvoiceMcpServerResolver;
 import com.hcd.telecomassistant.config.resolver.McpServerResolver;
 import com.hcd.telecomassistant.config.resolver.VendorMcpServerResolver;
 import io.modelcontextprotocol.client.transport.customizer.McpSyncHttpClientRequestCustomizer;
-import io.modelcontextprotocol.common.McpTransportContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.ai.mcp.client.common.autoconfigure.properties.McpStreamableHttpClientProperties;
-import org.springframework.ai.mcp.customizer.McpSyncClientCustomizer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -74,11 +72,5 @@ public class SecurityConfig {
                     .resolve(endpoint)
                     .ifPresent(apiKeyHeader -> builder.header(apiKeyHeader.name(), apiKeyHeader.value()));
         };
-    }
-
-    @Bean
-    McpSyncClientCustomizer syncClientCustomizer() {
-        return (name, spec) -> spec.transportContextProvider(() ->
-                McpTransportContext.create(Map.of("user", "horatiu")));
     }
 }
