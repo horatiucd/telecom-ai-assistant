0f0c08cd74cf15aec7641193120831ef8eff9b16
horatiu.dan <horatiu.dan@tangoe.com>
2026-04-16 13:31:02 +0300

    Removes unused import.

--- telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
+++ telecom-assistant/src/main/java/com/hcd/telecomassistant/config/SecurityConfig.java
@@ -13,8 +13,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Map;
-
 //TODO 11 - Security configuration
 //@Configuration
 @EnableConfigurationProperties({McpServerApiKeyProperties.class})
